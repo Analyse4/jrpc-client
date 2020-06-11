9dc1c7a460d2a03bd6e5ae18fe704b2d3672ca4c
Analyse4 <analysek4@gmail.com>
2020-06-11 18:55:09 +0800

    complete message SimpleReq/Ack and corresponding mechnism

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/Analyse4/jrpc-client/protocol"
 	"github.com/Analyse4/jrpc-client/stub"
 )
 
 func main() {
-	ack, err := stub.Send(":4242", "jrpcserver.ack", "req")
+	ack, err := stub.Send(":4241", "jrpc.simplehandler", "req")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(ack)
+	sa := new(protocol.SimpleAck)
+	err = json.Unmarshal(ack.Msg, sa)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sa.Content)
 }
--- stub/stub.go
+++ stub/stub.go
@@ -1,6 +1,7 @@
 package stub
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net"
@@ -12,41 +13,51 @@ import (
 const maxBufferSize = 1024
 
 // Send xxx
-func Send(addr, id, msg string) (string, error) {
+func Send(addr, id, msg string) (*protocol.BaseMsg, error) {
+	data := new(protocol.SimpleReq)
+	data.Content = msg
+	tdata, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
 	bm := new(protocol.BaseMsg)
+	bm.Msg = make([]byte, 0)
 	bm.ID = id
-	bm.Msg = []byte(msg)
+	bm.Msg = append(bm.Msg, tdata...)
+	fmt.Println(bm)
 
 	jmsg, err := json.Marshal(bm)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
+	fmt.Println(len(jmsg))
 	raddr, err := net.ResolveUDPAddr("udp", addr)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	conn, err := net.DialUDP("udp", nil, raddr)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer conn.Close()
 	n, err := conn.Write(jmsg)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	fmt.Printf("%v  datagram written: bytes: %d\n", time.Now(), n)
 
 	buffer := make([]byte, maxBufferSize)
 	n, saddr, err := conn.ReadFrom(buffer)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	fmt.Printf("%v  datagram received: bytes: %d, from: %s\n", time.Now(), n, saddr.String())
 
-	//var ack string
-	//err = json.Unmarshal(buffer, &ack)
+	buffer = bytes.Trim(buffer, "\x00")
+	err = json.Unmarshal(buffer, bm)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(buffer), nil
+	return bm, nil
 }
